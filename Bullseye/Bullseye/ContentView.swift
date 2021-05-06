e22d78bcf79fcc687e717492e2684ccee3da52b9
yoosoony <ystar819@naver.com>
2021-05-07 00:39:31 +0900

    pop up alert 기능 추가

--- Bullseye/Bullseye/ContentView.swift
+++ Bullseye/Bullseye/ContentView.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //the pop up alert가 visible 여부의 상관없이 선언
+    //@STATE: this code means 'we want SwiftUI to automatically recompute the body'
+    //private: this variable is private to content view and other objects and other structures, should not be able to access it.
+    //var: this variable means 'can be change'
+    //이름 적고
+    //: type
+    //= value
+    
+    @State private var alertIsVisible: Bool = false
+    
     var body: some View {
         VStack {
             Text("PUT THE BULLSEYE \nAS CLOSE AS YOU CAN DO\n🤪")
@@ -24,12 +35,21 @@ struct ContentView: View {
             HStack {
                 Text("1").bold()
                 Slider(value: .constant(50.0), in: 1.0...100.0)
-                Text("100").bold()
+                Text("100").bold() 
             }
-            Button(action: {}) {
+            Button(action: {
+                print("hello, there")
+                //self: this랑 비슷한 것 같아
+                self.alertIsVisible = true
+            }) {
                 Text("Hit me")
             }
-        }
+            .alert(isPresented: $alertIsVisible,
+                   content: {
+                    return Alert(title: Text("먼지알지?"),
+                    message: Text("mgrg?"),
+                    dismissButton: .default(Text("rg!")))
+        })
     }
 }
 
@@ -39,4 +59,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
             .previewLayout(.fixed(width: 568, height: 320))
     }
+}
 }
