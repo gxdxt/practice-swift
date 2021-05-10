f5535c3a591fed45aaf52489d8ec7a6bf475ffeb
yoosoony <ystar819@naver.com>
2021-05-11 07:52:04 +0900

    Add MyAlbum proj.

---
+++ MyAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by 유승태 on 2021/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
