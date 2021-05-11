052fbdf4b946fb769a5bb9b6d5512bcd69b96080
yoosoony <ystar819@naver.com>
2021-05-11 23:19:47 +0900

    add an alert function to the button

--- MyAlbum/ViewController.swift
+++ MyAlbum/ViewController.swift
@@ -11,9 +11,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func hello(_ sender: Any) {
+        let alert = UIAlertController(title: "hello", message: "my first app!", preferredStyle: .alert)
+        //alert라는 모양을 만들고
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil) //nil은 아무것도 핸들해주지 않는다.는 의미
+        //action이라는 모양과 기능을 추가하고
+        
+        alert.addAction(action)
+        //alert모양 안에 action이라는 action을 추가하고
+        
+        present(alert, animated: true, completion: nil)
+        //화면에 출력한다.
+    }
+    
 }
 
