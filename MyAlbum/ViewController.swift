796717a0a011221c38a71ca0bd2805d20b3aecc3
yoosoony <ystar819@naver.com>
2021-05-13 07:48:03 +0900

    가격 변수 설정

--- MyAlbum/ViewController.swift
+++ MyAlbum/ViewController.swift
@@ -8,14 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
     @IBAction func hello(_ sender: Any) {
-        let alert = UIAlertController(title: "hello", message: "my first app!", preferredStyle: .alert)
+        let message = "가격은 ₩\( currentValue)입니다." //String에 var 담기
+        //String Interplation??
+        
+        let alert = UIAlertController(title: "hello", message: message, preferredStyle: .alert)
         //alert라는 모양을 만들고
         
         let action = UIAlertAction(title: "OK", style: .default, handler: nil) //nil은 아무것도 핸들해주지 않는다.는 의미
@@ -26,18 +30,21 @@ class ViewController: UIViewController {
         
         present(alert, animated: true, completion: nil)
         //화면에 출력한다.
+        
+        //계속해서 변하는 값 설정
+       let randomPrice =  arc4random_uniform(10000)+1
+        //0~10000까지의 값들 중 랜덤하게 설정, 0인 값이 나오면 재미 없으니 +1 추가
+        //randomPrice의 값을 currentValue에 추가
+        currentValue = Int(randomPrice)
+        //***여기서 질문은, 언제 randomPrice값이 변화하는가? -- 이 함수가 속해있는 함수가 호출될 때마다!!! (여기선 Alert 창이 뜰 때마다)
+        
+        
+        
+        
     }
     
     
-    @IBAction func lemaire(_ sender: Any) {
-        let alert = UIAlertController(title: "Christophe Lemaire", message: "French", preferredStyle: .alert)
-        
-        let buy = UIAlertAction(title: "buy", style: .default, handler: nil)
-        
-        alert.addAction(buy)
-        
-        present(alert, animated: true, completion: nil)
-    }
+    
     
 }
 
