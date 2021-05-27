e54699e7236b67892ee7199b74a0f789b90b613f
yoosoony <ystar819@naver.com>
2021-05-28 01:12:02 +0900

    UIListCell is added with photos

--- BountyList/BountyViewController.swift
+++ BountyList/BountyViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    //List에 뿌려줄 이름 설정
+    let brandList = ["LEMAIRE", "Celine", "Maison Margiela", "Auralee", "Alyx", "JJJJound", "BBC", "Stussy"]
+    let designerList = ["Christophe Lemaire", "Phoebe Philo", "Martin Margiela", "A Japanese", "MMW", "Justin Saunders", "Pharrell Williams", "Ralph Diaz"]
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,17 +22,39 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
     //UITableViewDataSource를 사용하기 위해 필요한 코드
     //몇 개니?
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        //브랜드 개수만큼 출력
+        return brandList.count
     }
     
     //재활용되는 cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //guard쓰면 에러나네?
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+//            return UITableViewCell()
+//        }
+//        let img = UIImage(named: "\(brandList[indexPath.row]).jpeg")
+//    cell.imageView?.image = img
+//    cell.brandLabel.text = brandList[indexPath.row]
+//    cell.designerLabel.text = "\(designerList[indexPath.row])" //여긴 왜 불러오지??
+//
+//        return cell
+        //이거랑 밑에 if문이랑 동일한 코드
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+       if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell {
+            let img = UIImage(named: "\(brandList[indexPath.row]).jpeg")
+        cell.imageView?.image = img
+        cell.brandLabel.text = brandList[indexPath.row]
+        cell.designerLabel.text = "\(designerList[indexPath.row])" //여긴 왜 불러오지??
+        
+            return cell
+        } else {
+            return UITableViewCell()
+        }
         //identifier를 cell 로 지었으니, 해당 'table view cell'의 prop.으로 가서 cell로 변경해야 한다.
         //indexPath는 cell의 위치를 의미
-        
-        return cell
+        //현재 UITableCell인데, 우리는 UIListCell을 쓰고싶다. -> Casting
+
     }
     
     //이걸 다 해도, 코드랑 뷰랑 연결을 안해놓으면 안보인다.
@@ -48,4 +75,13 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
     }
     */
 
+}
+
+class ListCell: UITableViewCell {
+    //Cell 안에 들어갈 Element들
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var brandLabel: UILabel!
+    @IBOutlet weak var designerLabel: UILabel!
+    
+    // identity inspector에서 class 이름으로 해당 이름 변경해줘야해
 }
