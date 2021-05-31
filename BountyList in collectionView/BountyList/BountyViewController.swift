543b7896993a209dd5f76e43a2286be4f9d10012
yoosoony <ystar819@naver.com>
2021-06-01 07:53:49 +0900

    change tableView to collectionView

---
+++ BountyList in collectionView/BountyList/BountyViewController.swift
@@ -0,0 +1,203 @@
+//
+//  BountyViewController.swift
+//  BountyList
+//
+//  Created by yoosoony on 2021/05/27.
+//
+
+import UIKit
+
+class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // MVVM
+    
+    // Model
+    // - BountyInfo라는 object
+    // > BountyInfo 생성!
+    
+    // View
+    // - ListCell
+    // > ListCell에 필요한 정보를 ViewModel로부터 받아야 한다.
+    //
+    
+    // ViewModel
+    // - ViewController가 직접적으로 access하면 안되기 때문에, 생성
+    // > BountyViewModel 생성!
+    // > View Layer에서 필요한 Method 생성!
+    // > Model 소유! (BountyInfo's'를 가지고 있어야 한다.)
+    
+//    //밑에 MVVM 적용을 위해 대체할 let 선언
+//    let bountyInfoList: [BountyInfo] = [
+//        BountyInfo(brand: "LEMAIRE", designerName: "Christophe Lemaire"),
+//        BountyInfo(brand: "Celine", designerName: "Phoebe Philo"),
+//        BountyInfo(brand: "Maison Margiela", designerName: "Martin Margiela"),
+//        BountyInfo(brand: "Auralee", designerName: "A Japanese"),
+//        BountyInfo(brand: "Alyx", designerName: "MMW"),
+//        BountyInfo(brand: "JJJJound", designerName: "Justin Saunders"),
+//        BountyInfo(brand: "BBC", designerName: "Pharrell Williams"),
+//        BountyInfo(brand: "Stussy", designerName: "Ralph Diaz")
+//    ]
+    
+    let viewModel = BountyViewModel()
+    
+    //List에 뿌려줄 이름 설정
+//    let brandList = ["LEMAIRE", "Celine", "Maison Margiela", "Auralee", "Alyx", "JJJJound", "BBC", "Stussy"]
+//    let designerList = ["Christophe Lemaire", "Phoebe Philo", "Martin Margiela", "A Japanese", "MMW", "Justin Saunders", "Pharrell Williams", "Ralph Diaz"]
+    
+    //segueway 함수 실행 직전 준비하는 함수
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //detailViewController에게 Data를 준다.
+        if segue.identifier == "showDetail" {
+            let vc = segue.destination as? DetailViewController
+            if let index = sender as? Int {
+                
+                let bountyInfo = viewModel.bountyInfo(at: index)
+                
+//                vc?.brand = brandList[index]
+//                vc?.designerName = designerList[index]
+                vc?.viewModel.bountyInfo = bountyInfo
+                
+//                vc?.brand = bountyInfo.brand
+//                vc?.designerName = bountyInfo.designerName
+            }
+            
+        }
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    //UITableViewDataSource를 사용하기 위해 필요한 코드
+    //몇 개니?
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //브랜드 개수만큼 출력
+        //return brandList.count
+        return viewModel.numOfBountyInfoList
+    }
+    
+    //재활용되는 cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //guard쓰면 에러나네?
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+            return UITableViewCell()
+        }
+        //View 선언?
+        let bountyInfo = viewModel.bountyInfo(at: indexPath.row)
+        
+        //let img = UIImage(named: "\(brandList[indexPath.row]).jpeg")
+//        cell.imgView.image = img
+//        cell.brandLabel.text = brandList[indexPath.row]
+//        cell.designerLabel.text = "\(designerList[indexPath.row])" //여긴 왜 불러오지??
+        
+//        cell.imgView.image = bountyInfo.image
+//        cell.brandLabel.text = bountyInfo.brand
+//        //cell.designerLabel.text = bountyInfo.designerName 이렇게 왜 안하지?
+//        cell.designerLabel.text = "\(bountyInfo.designerName)"
+        cell.update(info: bountyInfo)
+        
+        return cell
+        //이거랑 밑에 if문이랑 동일한 코드
+        
+//
+//       if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell {
+//            let img = UIImage(named: "\(brandList[indexPath.row]).jpeg")
+//        cell.imgView?.image = img
+//        cell.brandLabel.text = brandList[indexPath.row]
+//        cell.designerLabel.text = "\(designerList[indexPath.row])" //여긴 왜 불러오지??
+//
+//            return cell
+//        } else {
+//            return UITableViewCell()
+//        }
+        //identifier를 cell 로 지었으니, 해당 'table view cell'의 prop.으로 가서 cell로 변경해야 한다.
+        //indexPath는 cell의 위치를 의미
+        //현재 UITableCell인데, 우리는 UIListCell을 쓰고싶다. -> Casting
+
+    }
+    
+    //이걸 다 해도, 코드랑 뷰랑 연결을 안해놓으면 안보인다.
+    //해당 table view의 delegate가 해당 컨트롤러다 라고 세팅해야해
+    
+    //클릭됐을 때 어떻게 할꺼야? -> UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("I'm just clicked --> \(indexPath.row)")
+        
+        //Segueway 실행
+        performSegue(withIdentifier: "showDetail", sender: indexPath.row) //몇 번째 cell이다!
+        
+        //click 되었을 때, 색 변하는 것은 selection!
+        
+        //몇 번째 cell이 클릭되었는지 알 수 있는 곳 -> indexPath를 가지고 있어서
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+   
+}
+
+class ListCell: UITableViewCell {
+    //Cell 안에 들어갈 Element들
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var brandLabel: UILabel!
+    @IBOutlet weak var designerLabel: UILabel!
+    
+    // identity inspector에서 class 이름으로 해당 이름 변경해줘야해
+    
+    
+    //Controller가 가지고 있을 필요가 없어서 해당 class로 끌고 내려왔다.
+    func update(info: BountyInfo) {
+        imgView.image = info.image
+        brandLabel.text = info.brand
+        //cell.designerLabel.text = bountyInfo.designerName 이렇게 왜 안하지?
+        designerLabel.text = "\(info.designerName)"
+        
+        //이제 위에 부분에 이 함수 불러야해.
+    }
+}
+
+
+// ViewModel이 model을 가지고 있어야 한다.
+class BountyViewModel {
+    //밑에 MVVM 적용을 위해 대체할 let 선언
+    let bountyInfoList: [BountyInfo] = [
+        BountyInfo(brand: "LEMAIRE", designerName: "Christophe Lemaire"),
+        BountyInfo(brand: "Celine", designerName: "Phoebe Philo"),
+        BountyInfo(brand: "Maison Margiela", designerName: "Martin Margiela"),
+        BountyInfo(brand: "Auralee", designerName: "A Japanese"),
+        BountyInfo(brand: "Alyx", designerName: "MMW"),
+        BountyInfo(brand: "JJJJound", designerName: "Justin Saunders"),
+        BountyInfo(brand: "BBC", designerName: "Pharrell Williams"),
+        BountyInfo(brand: "Stussy", designerName: "Ralph Diaz")
+    ]
+    
+    var sortedList: [BountyInfo] {
+        let sortedList = bountyInfoList.sorted { prev, next in
+            return prev.brand < next.brand
+        }
+        return sortedList
+    }
+    
+    //count를 담을 변수 선언
+    var numOfBountyInfoList: Int {
+        return bountyInfoList.count
+    }
+    
+    //index 및 indexPath.row 접근할 함수 선언
+    func bountyInfo(at index: Int) -> BountyInfo {
+        return sortedList[index]
+    }
+}
+
+
---
+++ BountyList in collectionView/BountyList/DetailViewController.swift
@@ -0,0 +1,89 @@
+//
+//  DetailViewController.swift
+//  BountyList
+//
+//  Created by yoosoony on 2021/05/28.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    // MVVM
+    
+    // Model
+    // - BountyInfo라는 object
+    // > BountyInfo 생성!
+    
+    // View
+    // - image View, nameLabel, bountyLabel
+    // > View들은 ViewModel를 통해서 구성되어야 한다.
+    //
+    
+    // ViewModel
+    // - ViewController가 직접적으로 access하면 안되기 때문에, 생성
+    // > View Layer에서 필요한 Method 생성!
+    // > Model 소유! (BountyInfo's'를 가지고 있어야 한다.)
+    
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var brandLabel: UILabel!
+    @IBOutlet weak var designer: UILabel!
+
+    //이름이랑 현상금 정보만 있으면, 사진을 가져올 수 있다?
+//    var brand: String? // optional로 선언
+//    var designerName: String?
+    
+//    var bountyInfo: BountyInfo?
+    let viewModel = DetailBountyViewModel()
+    
+    //view가 memory에 올라온 시점에 실행되는 함수
+    //보이기 바로 직전
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //보이기 바로 직전에 updateUI 함수 실행
+        updateUI()
+        
+    }
+    
+    //updateUI 이름 정보를 받아와서 UI 변경
+    func updateUI(){
+        
+        if let bountyInfo = viewModel.bountyInfo {
+                    imgView.image = bountyInfo.image
+                    brandLabel.text = bountyInfo.brand
+                    designer.text = "\(bountyInfo.designerName)"
+                } 
+//        if let bountyInfo = self.bountyInfo {
+//            imgView.image = bountyInfo.image
+//            brandLabel.text = bountyInfo.brand
+//            designer.text = "\(bountyInfo.designerName)"
+//        } -----------> 이건 직접 접근하니까 앞으로 ViewModel로 통하게 만들자
+        
+//        if let brand = self.brand, let designerName = self.designerName {
+//            let img = UIImage(named: "\(brand).jpeg")
+//        imgView.image = img
+//        brandLabel.text = brand
+//        designer.text = designerName
+//        }
+    }
+    
+    //버튼 클릭 시, 닫히는 액션
+    @IBAction func close(_ sender: Any) {
+        //사라진다 명령어
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //두 개의 ViewController를 연결시켜주는 Segueway
+    //Bounty View Controller 좌측 control > Present Modally
+    // 새로 생긴 line에 identifier 정하기
+    // 이제 기본 ViewController에서 해당 Segueway 실행 함수 작성 ---
+    
+}
+
+class DetailBountyViewModel {
+    var bountyInfo: BountyInfo?
+    
+    func update(model: BountyInfo?) { //매번 여기를 통할 것이기 때문에
+        bountyInfo = model
+    }
+}
