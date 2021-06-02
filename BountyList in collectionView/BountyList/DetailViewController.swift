c2e44e39ce1c9e6fe09307fbd06ee558d19f4a7c
yoosoony <ystar819@naver.com>
2021-06-02 23:03:27 +0900

    added Animating

--- BountyList in collectionView/BountyList/DetailViewController.swift
+++ BountyList in collectionView/BountyList/DetailViewController.swift
@@ -28,7 +28,9 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var brandLabel: UILabel!
     @IBOutlet weak var designer: UILabel!
-
+    @IBOutlet weak var brandLabelCenterX: NSLayoutConstraint!
+    @IBOutlet weak var designerLabelCenterX: NSLayoutConstraint!
+    
     //이름이랑 현상금 정보만 있으면, 사진을 가져올 수 있다?
 //    var brand: String? // optional로 선언
 //    var designerName: String?
@@ -38,12 +40,51 @@ class DetailViewController: UIViewController {
     
     //view가 memory에 올라온 시점에 실행되는 함수
     //보이기 바로 직전
-    override func viewDidLoad() {
+    override func viewDidLoad() { //Memory가 올라왔다는 것을 보여주는 함수랄까
         super.viewDidLoad()
         //보이기 바로 직전에 updateUI 함수 실행
         updateUI()
+        //애니메이션 준비
+        prepareAnimation()
         
     }
+    
+    //보여졌다!는 함수는 이거야
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //여기에 애니메이션 들아간다.
+        showAnimation()
+    }
+    
+    private func prepareAnimation() {
+        //constant 값이 커지면 View가 우측으로 간다.
+        brandLabelCenterX.constant = view.bounds.width
+        designerLabelCenterX.constant = view.bounds.width
+    }
+    
+    private func showAnimation() {
+        brandLabelCenterX.constant = 0
+        designerLabelCenterX.constant = 0
+        
+        //layout 자체가 animating이 되도록!
+//        UIView.animate(withDuration: 0.3) {
+//            //지금 constant값(layout)이 변경되었으니까 이거 다시 바꿔줘! 라는 코드
+//            self.view.layoutIfNeeded()
+//        }
+        
+        //더 디테일하게
+//        UIView.animate(withDuration: 0.3, delay: 0.1,
+//            options: .curveEaseIn, animations: {self.view.layoutIfNeeded()}, completion: nil)
+ //더 더 디테일하게
+        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: {self.view.layoutIfNeeded()}, completion: nil)
+        
+        UIView.transition(with: imgView, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+    }
+
+    
+    
+    
     
     //updateUI 이름 정보를 받아와서 UI 변경
     func updateUI(){
