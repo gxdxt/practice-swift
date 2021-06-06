3bd6d1078f924f99c9548ddf3b24419c8d3579fe
yoosoony <ystar819@naver.com>
2021-06-06 14:46:56 +0900

    update track load

--- AppleMusicStApp/Track/TrackManager.swift
+++ AppleMusicStApp/Track/TrackManager.swift
@@ -18,12 +18,35 @@ class TrackManager {
     // TODO: 생성자 정의하기
     init() {
 //        let playerItem = AVPlayerItem(url: <#T##URL#>) //곡에 대한 url을 이용해서 player object를 생성!
-        
+        // 1. 트랙 로드
+        let tracks = loadTracks()
+        self.tracks = tracks
+        self.album = loadAlbums(tracks: tracks)
+        self.todaysTrack = self.tracks.randomElement() //모든 Array에서 사용할 수 있는 랜덤 함수
     }
 
     // TODO: 트랙 로드하기
     func loadTracks() -> [AVPlayerItem] {
-        return []
+        // 파일을 읽어서 AVPlayer 형태로 만들기
+        let urls = Bundle.main.urls(forResourcesWithExtension: "mp3", subdirectory: nil) ?? [] //Bundle = this App, mp3 파일 찾고 없으면 깡통 (default가 optional)
+        //url이 여러 개 일 때
+//        var items: [AVPlayerItem] = []
+//        for url in urls {
+//            let item = AVPlayerItem(url: url)
+//            items.append(item)
+//        }
+        
+//        let items = urls.map { url -> AVPlayerItem in //고계함수? '-> AVPlayerItem 삭제 가능'
+//            let item = AVPlayerItem(url: url) // { AVPlayerItem(url: $0 } 로 아예 변경도 된다.
+//            return item
+//        }
+        
+        //이것도 명확한 표현법인 듯
+        let items = urls.map { url in
+            return AVPlayerItem(url: url)
+        }
+        
+        return items
     }
     
     // TODO: 인덱스에 맞는 트랙 로드하기
