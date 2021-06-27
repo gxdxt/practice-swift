6eeb454da1f164848dbb362ffedd34e65318c8d4
yoosoony <ystar819@naver.com>
2021-06-28 06:58:14 +0900

    URLSession's properties in the playground

--- playground/URLSession.playground/Contents.swift
+++ playground/URLSession.playground/Contents.swift
@@ -5,3 +5,32 @@ import UIKit
 let urlString = "https://ituens.apple.com/search?media=music&unitiy=song&term=theweeknd"
 let url = URL(string: urlString)
 
+//현재 urlString
+url?.absoluteString
+
+//어떤 방식으로 네트워킹 하는가
+url?.scheme
+
+url?.host
+
+url?.path
+
+url?.query
+
+url?.baseURL
+
+//base URL이 나오게
+let baseURL = URL(string: "https://ituens.apple.com")
+let relativeURL = URL(string: "search?media=music&unitiy=song&term=theweeknd", relativeTo: baseURL)
+
+relativeURL?.absoluteString
+
+relativeURL?.scheme
+
+relativeURL?.host
+
+relativeURL?.path
+
+relativeURL?.query
+
+relativeURL?.baseURL
