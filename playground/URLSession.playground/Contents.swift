c5c290c8eedc105869f27b195e6783123e1c2ae0
yoosoony <ystar819@naver.com>
2021-06-28 20:14:11 +0900

    URLComponents in the playground

--- playground/URLSession.playground/Contents.swift
+++ playground/URLSession.playground/Contents.swift
@@ -20,7 +20,7 @@ url?.query
 url?.baseURL
 
 //base URL이 나오게
-let baseURL = URL(string: "https://ituens.apple.com")
+let baseURL = URL(string: "https://itunes.apple.com")
 let relativeURL = URL(string: "search?media=music&unitiy=song&term=theweeknd", relativeTo: baseURL)
 
 relativeURL?.absoluteString
@@ -34,3 +34,17 @@ relativeURL?.path
 relativeURL?.query
 
 relativeURL?.baseURL
+
+
+// URLComponents >> 왜 쓴다고??
+
+var urlComponets = URLComponents(string: "https://itunes.apple.com/search?")
+let mediaQuery = URLQueryItem(name: "media", value: "music")
+let entityQuery = URLQueryItem(name: "unity", value: "song")
+let termQuery = URLQueryItem(name: "term", value: "theweeknd")
+
+urlComponets?.queryItems?.append(mediaQuery)
+urlComponets?.queryItems?.append(entityQuery)
+urlComponets?.queryItems?.append(termQuery)
+
+
