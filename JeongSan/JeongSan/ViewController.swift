567f35a9508abbf72d6285ade3db11eee473d4f2
yoosoony <ystar819@naver.com>
2021-07-07 23:12:39 +0900

    proj. jeongsan starts

---
+++ JeongSan/JeongSan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JeongSan
+//
+//  Created by 유승태 on 2021/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
