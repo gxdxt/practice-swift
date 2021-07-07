17adc401499bc30ffc24a7e6a3a18bf3ea678692
yoosoony <ystar819@naver.com>
2021-07-07 23:18:06 +0900

    design the pages and functions

--- JeongSan/JeongSan/ViewController.swift
+++ JeongSan/JeongSan/ViewController.swift
@@ -13,6 +13,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //첫 번째 화면에서 stepper를 이용해 총 인원 수 값 받기
+    
+    //두 번째 화면에서 해당 인원에 맞게끔 인원 collection 생성
+        //필요한 정보는 '이름', '지불한 금액'
+    
+    //세 번째 화면에서 결과값 출력
+        //총 금액
+        //'누가' '누구에게' 돈을 '얼마나' 지불해야 하는지 출력
+    
+    
+    //common
+    //다음 페이지로 / 이전 페이지로 이동하는 버튼
+    
+    //first page
+    //stepper로 숫자 올리기, text 직접 입력도 가능하게
+    
+    //second page
+    //금액 입력했을 때 자동으로 , 생성
+    
 
 
 }
