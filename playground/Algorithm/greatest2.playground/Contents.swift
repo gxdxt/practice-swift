36357e053f4aaa7861c5536397d3ce7b7a98d845
yoosoony <ystar819@naver.com>
2021-07-20 21:36:31 +0900

    Add the comments for the abbreviation

--- playground/Algorithm/greatest2.playground/Contents.swift
+++ playground/Algorithm/greatest2.playground/Contents.swift
@@ -5,7 +5,14 @@ func solution(_ numbers:[Int]) -> String {
     if sorted[0] == 0 {
         return "0"
     }
+    sorted
     return sorted.reduce("") {
         $0 + "\($1)"
     }
+    //reduce("", { (s1: Int, s2: Int) -> String in return "\(s1)" + "\(s2)" }
+    //reduece("") { (s1: Int, s2: Int) -> String in return "\(s1)" + "\(s2)" }
+    //reduce("") { (s1, s2) in "\(s1)" + "\(s2)" }
+    //reduce("") { $0 + "\($1)" }
 }
+
+solution([3, 30, 34, 5, 9])
